92ae22871e2cc32017cc063c9a6c6979a456c4a4
kabust <tulyulyuk91@gmail.com>
2025-04-25 22:50:26 +0200

    added verbose_name_plural to category model

--- category/models.py
+++ category/models.py
@@ -5,3 +5,6 @@ class Category(models.Model):
     name = models.CharField(max_length=63, unique=True)
     is_for_male = models.BooleanField(default=False)
     is_for_female = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Categories"
