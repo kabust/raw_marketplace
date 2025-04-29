a6534fea3b3e73338058afdefb9b8f70d200f5ba
Alex <delete.rap.stupid@gmail.com>
2025-04-29 22:31:28 +0200

    add permissions

--- product/views.py
+++ product/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, status
 
 from product.models import Product, Option, Category, Image
+from product.permissions import IsAdminOrReadOnly
 from product.serializers import ProductSerializer, OptionSerializer, CategorySerializer, ImageSerializer, \
     ProductListSerializer, ProductDetailSerializer
 
@@ -8,15 +9,18 @@ from product.serializers import ProductSerializer, OptionSerializer, CategorySer
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.prefetch_related("options", "images").select_related("category", "main_image")
+    permission_classes = (IsAdminOrReadOnly,)
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -31,3 +35,4 @@ class ProductViewSet(viewsets.ModelViewSet):
 class OptionViewSet(viewsets.ModelViewSet):
     queryset = Option.objects.all()
     serializer_class = OptionSerializer
+    permission_classes = (IsAdminOrReadOnly,)
