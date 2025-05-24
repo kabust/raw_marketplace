37e41be092e949e5a9b631237b38b4c3db9e038e
Alex <delete.rap.stupid@gmail.com>
2025-05-24 13:31:04 +0200

    add user login

--- user/views.py
+++ user/views.py
@@ -1,10 +1,18 @@
+from oauth2_provider.settings import oauth2_settings
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from oauth2_provider.contrib.rest_framework import OAuth2Authentication, TokenHasReadWriteScope
 
 from user.models import User
-from user.serializers import UserSerializer, UserRegistrationSerializer, PasswordUpdateSerializer
+from user.serializers import (
+    UserSerializer,
+    UserRegistrationSerializer,
+    PasswordUpdateSerializer,
+    LoginResponseSerializer,
+    UserLoginSerializer,
+)
+from user.utils import get_authentication_code
 
 
 class UserViewSet(viewsets.GenericViewSet):
@@ -16,6 +24,7 @@ class UserViewSet(viewsets.GenericViewSet):
         "register": UserRegistrationSerializer,
         "set_password": PasswordUpdateSerializer,
         "me": UserSerializer,
+        "login": UserLoginSerializer,
     }
 
     def get_serializer_class(self):
@@ -39,3 +48,19 @@ class UserViewSet(viewsets.GenericViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({"message": "Password updated successfully."})
+
+    @action(detail=False, methods=["post"], permission_classes=[permissions.AllowAny])
+    def login(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        access_token, refresh_token = get_authentication_code(user.email)
+        data = {
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+            "token_type": "Bearer",
+            "expires_in": oauth2_settings.ACCESS_TOKEN_EXPIRE_SECONDS,
+        }
+        response_serializer = LoginResponseSerializer(instance=data)
+
+        return Response(response_serializer.data, status=status.HTTP_200_OK)
